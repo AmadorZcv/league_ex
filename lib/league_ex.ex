26ab9b392ebff67ca4e5036fc3fc37dc5db37b01
AmadorZcv <lucasamadorzcv@gmail.com>
2021-12-01 19:29:07 -0300

    feat: mix project start

---
+++ lib/league_ex.ex
@@ -0,0 +1,18 @@
+defmodule LeagueEx do
+  @moduledoc """
+  Documentation for `LeagueEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LeagueEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/league_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule LeagueExTest do
+  use ExUnit.Case
+  doctest LeagueEx
+
+  test "greets the world" do
+    assert LeagueEx.hello() == :world
+  end
+end
